d3fa14e73dd077084c92ec60fd22d7f690b11f0c
Sanjuvaru29 <sanjuvaru29@gmail.com>
2021-06-20 18:21:53 +0530

    Removed node_module folder

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend:{
+      colors: {
+        primary: {
+          100:'#181A25',
+          200:'#1D1F2B',
+        },
+        secondary: {
+          100: '#FFFFFF',
+          200: '',
+        }
+    },
+    width:{
+      '100':'23%',
+      '105':'30%',
+      '110':'55%'
+    }
+      
+    },
+  }, 
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
